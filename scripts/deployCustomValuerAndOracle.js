36b361207ab79b270263c3e9b9d2168f3a07a81e
bitsandtea <gashi.dite@gmail.com>
2021-04-02 13:27:54 +0200

    scripts/deployCustomValuerAndOracle

--- scripts/deployCustomValuerAndOracle.js
+++ scripts/deployCustomValuerAndOracle.js
@@ -1,10 +1,10 @@
+
 /*  
     controller address KOVAN for SCIFI: 0x9048278ca7e874f9338e4898c436ab07aa454701
-    Kovan Oracle: 0x5547694dac65f5046c02ffd3875bdf46f3bab713
+    Kovan Oracle: 0x5547694DaC65f5046C02FfD3875Bdf46F3Bab713
     WETH Kovan: 0xa1c74a9a3e59ffe9bee7b85cd6e91c0751289ebd
 
-    to run this deploy script with hardhat use: 
-    npx hardhat run --network localhost scripts/deployCustomValuerAndOracle.js
+    to run this deploy script with hardhat use: npx hardhat run --network localhost scripts/deployCustomValuerAndOracle.js
 
 */
 
