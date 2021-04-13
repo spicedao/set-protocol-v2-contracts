e225ad6e12fa97a36843fd38b668e03ca17bdba9
bitsandtea <gashi.dite@gmail.com>
2021-04-13 12:20:09 +0200

    Updated to USDC

--- scripts/deployCustomValuerAndOracle.js
+++ scripts/deployCustomValuerAndOracle.js
@@ -1,19 +1,16 @@
 
-/*  
-    controller address KOVAN for SCIFI: 0x9048278ca7e874f9338e4898c436ab07aa454701
-    Kovan Oracle: 0x5547694DaC65f5046C02FfD3875Bdf46F3Bab713
-    WETH Kovan: 0xa1c74a9a3e59ffe9bee7b85cd6e91c0751289ebd
 
-    to run this deploy script with hardhat use: npx hardhat run --network localhost scripts/deployCustomValuerAndOracle.js
-
+/*
+    to run this deploy script with hardhat use: 
+    npx hardhat run --network localhost scripts/deployCustomValuerAndOracle.js
 */
+const MAIN_ORACLE = "0x45aa3ae6c36b35b4c63d62476cbb13ed827317f0";
+const USDC = "0x7079f3762805cff9c979a5bdc6f5648bcfee76c8";
 
-const KOVAN_MAIN_ORACLE = "0x5547694dac65f5046c02ffd3875bdf46f3bab713";
-const KOVAN_WETH = "0xa1c74a9a3e59ffe9bee7b85cd6e91c0751289ebd";
 
 async function main() {
     const DIAPriceOracle = await ethers.getContractFactory("DIAPriceOracle");
-    const DIAPriceOracleDeployed = await DIAPriceOracle.deploy(KOVAN_WETH, KOVAN_MAIN_ORACLE);
+    const DIAPriceOracleDeployed = await DIAPriceOracle.deploy(USDC, MAIN_ORACLE);
 
     const SetValuer = await ethers.getContractFactory("CustomOracleSetValuer");
     const valuer = await SetValuer.deploy(DIAPriceOracleDeployed.address);
